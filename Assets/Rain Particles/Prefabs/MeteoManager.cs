8da3fad30e481cd4dd09f103e32ef90be6b7c981
BackDeRiz <103582055+ZacharyMeunier@users.noreply.github.com>
2026-03-22 15:08:17 -0400

    Added ui

---
+++ Assets/Rain Particles/Prefabs/MeteoManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+public class MeteoManager : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI temperatureText;
+    public GameObject sun;
+    public GameObject snow;
+    public GameObject rain;
+
+    void Start()
+    {
+        StartCoroutine(GetWeather());
+    }
+    IEnumerator GetWeather()
+    {
+        string url = "https://api.open-meteo.com/v1/forecast?latitude=45.50&longitude=-73.56&current_weather=true";
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            WeatherData data = JsonUtility.FromJson<WeatherData>(request.downloadHandler.text);
+            float temp = data.current_weather.temperature;
+            int code = data.current_weather.weathercode;
+            temperatureText.text = temp + "°C";
+            // Activation simple selon le code
+            snow.SetActive(code >= 71 && code <= 77);
+            //sun.SetActive(code == 0);
+            rain.SetActive(code >= 61 && code <= 65);
+        }
+        else
+        {
+            Debug.LogError(request.error);
+        }
+    }
+}
+[System.Serializable]
+public class WeatherData
+{
+    public CurrentWeather current_weather;
+}
+[System.Serializable]
+public class CurrentWeather
+{
+    public float temperature;
+    public int weathercode;
+}
